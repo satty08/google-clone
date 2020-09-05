a8e30070d607e617d1195df1b35d62e5ece07f02
Satyam <satyambhardawaj@gmail.com>
2020-09-05 15:24:10 +0530

    home page design complete

---
+++ src/Pages/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
+import Search from './Search';
+
+function Home() {
+    return (
+        <div>
+            <div className='home__header'>
+                <div className="home__headerLeft">
+                    <Link to="/about">About</Link>
+                    <Link to="/store">Store</Link>
+                </div>
+                <div className="home__headerRight">
+                    <Link to="/gmail">Gmail</Link>
+                    <Link to="/images">Images</Link>
+                    <AppsIcon />
+                    <Avatar />
+
+                </div>
+            </div>
+
+            <div className="home__body">
+                <img 
+                    src="https://www.google.com/logos/doodles/2015/googles-new-logo-5078286822539264.3-hp2x.gif"
+                    alt=""
+                />
+                <div className="home__inputContainer">
+                    <Search />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
